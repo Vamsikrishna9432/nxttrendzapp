5d66e7c03f410119e7b7db4f5feae623baa6d963
vamsikrishna9432 <krishnavamsi9432@gmail.com>
2022-11-15 12:37:01 +0000

    nxtTrendzEcom

---
+++ src/components/CartSummary/index.js
@@ -0,0 +1,32 @@
+// Write your code here
+import CartContext from '../../context/CartContext'
+
+import './index.css'
+
+const CartSummary = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      let total = 0
+      cartList.forEach(eachCartItem => {
+        total += eachCartItem.price * eachCartItem.quantity
+      })
+
+      return (
+        <div className="summary-container">
+          <div className="summary-details">
+            <h1 className="su-ti">
+              <span className="amount">Order Total: </span>Rs {total}/-
+            </h1>
+            <p className="no-items">{cartList.length} items in cart</p>
+            <button className="checkout-btn" type="button">
+              Checkout
+            </button>
+          </div>
+        </div>
+      )
+    }}
+  </CartContext.Consumer>
+)
+
+export default CartSummary
